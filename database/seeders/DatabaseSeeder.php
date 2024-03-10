930e19aaa153e38eb84b66d9db77e33805ef1d4d
marxlima <marxmaciel@gmail.com>
2024-03-10 15:45:33 -0300

    model e factory de task

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(50)->create();
         \App\Models\TaskGroup::factory(5)->create();
+        \App\Models\Task::factory(200)->create();
+
 
 
         // \App\Models\User::factory()->create([
